bd31ecd8646575e4a72231993d480091c6124815
Gaurav Seth <gseth@microsoft.com>
2017-10-23 14:47:15 +0530

    Commit 23 OCT

---
+++ NodeTodoListMongo/config.js
@@ -0,0 +1,8 @@
+﻿var config = {}
+
+config.host = process.env.HOST || "https://caddocdbsql.documents.azure.com:443/";
+config.authKey = process.env.AUTH_KEY || "HFULs6MtHcnx41IWfvekbVsFXPZM50yP5oK0ZjUz6imix0O50sirdaJp5VfFWUFyqOTbXhfT4b0SDFmB5FVa0A==";
+config.databaseId = "ToDoList";
+config.collectionId = "Items";
+
+module.exports = config;
